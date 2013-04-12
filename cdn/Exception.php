61822a47eba14d70f14ed395e8cadf088ad68e7b
codeantor <naregmailbox@yahoo.com>
2013-04-12 18:42:49 +0800

    htacess zasaw

--- cdn/Exception.php
+++ cdn/Exception.php
@@ -5,7 +5,7 @@ class Exception extends \Exception
 {
     public function errorMessage()
     {
-        $errorMsg = 'Error on line '.$this->getLine().' in '.$this->getFile().': <b>'.$this->getMessage().'</b>';
+        $errorMsg = 'Error on line ' . $this->getLine() . ' in ' . $this->getFile() . ': <b>' . $this->getMessage() . '</b>';
         return $errorMsg;
     }
 }
